c48c0e53c5064f7cbcded4e886d19a68e6b9af95
Dicky Darmawan <dicky93darmawan@gmail.com>
2022-08-29 21:31:16 +0800

    admin

--- lib/view/admin.dart
+++ lib/view/admin.dart
@@ -1,11 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
-
-import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 import '../model/mac_model.dart';
